4d38143a20664bba38d5f2fed491d9ef73b98037
Bryan Cattle <bryan.cattle@gmail.com>
2017-01-01 14:58:15 -0800

    Initial Commit

---
+++ AudioLogger-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioLogger-iOS
+//
+//  Created by Bryan on 1/1/17.
+//  Copyright © 2017 bcattle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
