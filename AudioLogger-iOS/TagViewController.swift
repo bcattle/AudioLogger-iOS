f42ccd1d491e6891a902bedb380ede8aa79ceefe
Bryan Cattle <bryan.cattle@gmail.com>
2017-01-02 02:06:07 -0800

    Initial commit of ALOpusEncoder, nearly done. Still need to hook up read_samples to delegate function.

--- AudioLogger-iOS/TagViewController.swift
+++ AudioLogger-iOS/TagViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+//import EZAudio
 
 class TagViewController: UIViewController {
 
---
+++ AudioLogger-iOS/ogg_encoder_example.c
@@ -0,0 +1,9 @@
+//
+//  ogg_encoder_example.c
+//  AudioLogger-iOS
+//
+//  Created by Bryan on 1/1/17.
+//  Copyright © 2017 bcattle. All rights reserved.
+//
+
+#include <stdio.h>
