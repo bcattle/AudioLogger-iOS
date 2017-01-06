221aaa39be6d79ac1d456e72ea3a12a62c463750
Bryan Cattle <bryan.cattle@gmail.com>
2017-01-06 02:57:47 -0800

    Cleaned Opus stuff out of the repo

---
+++ Pods/Target Support Files/TPCircularBuffer/TPCircularBuffer-umbrella.h
@@ -0,0 +1,10 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "TPCircularBuffer+AudioBufferList.h"
+#import "TPCircularBuffer.h"
+
+FOUNDATION_EXPORT double TPCircularBufferVersionNumber;
+FOUNDATION_EXPORT const unsigned char TPCircularBufferVersionString[];
+
